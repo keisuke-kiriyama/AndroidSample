b200d4259016138447e439ca7f52831e49f6d9e7
keisuke-kiriyama <krkr.xm.rkr7x@gmail.com>
2021-01-31 10:52:27 +0900

    send email

--- OurMenu/app/src/main/java/com/example/keisuke_kiriyama/ourmenu/MainActivity.kt
+++ OurMenu/app/src/main/java/com/example/keisuke_kiriyama/ourmenu/MainActivity.kt
@@ -1,18 +1,37 @@
 package com.example.keisuke_kiriyama.ourmenu
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mailBtn.setOnClickListener() {
+            sendMail()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main, menu)
         return true
+    }
+
+    private fun sendMail () {
+        val subject = "メールテスト"
+        val text = "このメールはテスト用のメールです。"
+        val uri = Uri.fromParts("mailto", "address@example.com", null)
+        val intent = Intent(Intent.ACTION_SENDTO, uri)
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+        intent.putExtra(Intent.EXTRA_TEXT, text)
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
     }
 }
