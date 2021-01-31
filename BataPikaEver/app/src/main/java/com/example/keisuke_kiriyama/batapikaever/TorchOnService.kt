4c9256f52f8c9d433ca079cfcdb7bd1d41d3825d
keisuke-kiriyama <krkr.xm.rkr7x@gmail.com>
2021-01-31 11:26:45 +0900

    途中

--- BataPikaEver/app/src/main/java/com/example/keisuke_kiriyama/batapikaever/TorchOnService.kt
+++ BataPikaEver/app/src/main/java/com/example/keisuke_kiriyama/batapikaever/TorchOnService.kt
@@ -1,10 +1,54 @@
 package com.example.keisuke_kiriyama.batapikaever
 
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.hardware.camera2.CameraManager
+import android.os.Handler
 import android.os.IBinder
 
-class TorchOnService : Service() {
+class TorchOnService : Service(), SensorEventListener {
+    private val threshold: Float = 10f
+    private var oldValue: Float = 0f
+    private lateinit var cameraManager: CameraManager
+    private var cameraID: String? = null
+    private var lightOn: Boolean = false
+    //private val tag = "Bata Pika!"
+
+    override fun onCreate() {
+        super.onCreate()
+        cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        cameraManager.registerTorchCallback(object: CameraManager.TorchCallback() {
+            override fun onTorchModeChanged(cameraId: String, enabled: Boolean) {
+                super.onTorchModeChanged(cameraId, enabled)
+                cameraID = cameraId
+                lightOn = enabled
+            }
+        }, Handler())
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+    }
 
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
